3b5f22ef1fdf8dbc9576d229effb6f04fc3971ec
zmeytee <zmeytee@gmail.com>
2022-03-20 16:02:59 +0300

    fix

--- app/src/main/java/ru/zmeytee/myapplication/MainActivity.kt
+++ app/src/main/java/ru/zmeytee/myapplication/MainActivity.kt
@@ -1,5 +1,7 @@
 package ru.zmeytee.myapplication
 
+import android.app.Activity
+import android.content.res.Configuration
 import android.os.Build
 import android.os.Bundle
 import android.view.View
@@ -18,9 +20,24 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
                     or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION)
         }
 
+        val isNightMode = resources.configuration.uiMode and
+                Configuration.UI_MODE_NIGHT_MASK == Configuration.UI_MODE_NIGHT_YES
+        setSystemUiVisibilityFlag(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR, !isNightMode)
+
         supportFragmentManager.beginTransaction()
             .replace(R.id.fragmentContainerView, ToolbarFragment::class.java, null)
             .commit()
     }
 
+}
+
+@Suppress("DEPRECATION")
+private fun Activity.setSystemUiVisibilityFlag(bits: Int, on: Boolean) {
+    var flags: Int = window.decorView.systemUiVisibility // get current flag
+    flags = if (on) {
+        flags or bits
+    } else {
+        flags and bits.inv()
+    }
+    window.decorView.systemUiVisibility = flags
 }
